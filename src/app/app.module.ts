9292d80bdfef53f7ae2999908e6a4dcea43c1319
tarangpandya <tarang.pandya@gmail.com>
2020-12-08 12:49:48 -0500

    separated  i18n module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { FormOneComponent } from './form-one/form-one.component';
 import { FormTwoComponent } from './form-two/form-two.component';
+import { I18nModule } from './i18n/i18n.module';
 
 @NgModule({
   declarations: [
@@ -18,21 +17,10 @@ import { FormTwoComponent } from './form-two/form-two.component';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: httpTranslateLoader,
-        deps: [HttpClient]
-      }
-    })
+    I18nModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 
 export class AppModule { }
-
-// AOT compilation support
-export function httpTranslateLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http);
-}
