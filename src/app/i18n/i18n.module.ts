7d50d4756f9b1184d75e8bbd2cc19978ec502baa
tarangpandya <tarang.pandya@gmail.com>
2020-12-08 14:17:31 -0500

    Added TranslateCacheService

--- src/app/i18n/i18n.module.ts
+++ src/app/i18n/i18n.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateCacheModule, TranslateCacheSettings, TranslateCacheService } from 'ngx-translate-cache';
 
 @NgModule({
   imports: [
@@ -13,17 +14,34 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
         deps: [HttpClient]
       }
     }),
+    TranslateCacheModule.forRoot({
+      cacheService: {
+        provide: TranslateCacheService,
+        useFactory: translateCacheFactory,
+        deps: [TranslateService, TranslateCacheSettings]
+      },
+      cacheMechanism: 'Cookie'
+    })
   ],
   exports: [TranslateModule]
 })
 export class I18nModule {
-  constructor(translate: TranslateService) {
+  constructor(translate: TranslateService,
+  translateCacheService: TranslateCacheService) {
+    translateCacheService.init();
     translate.addLangs(['en', 'es']);
-    const browserLang = translate.getBrowserLang();
+    const browserLang = translateCacheService.getCachedLanguage() || translate.getBrowserLang();
     translate.use(browserLang.match(/en|es/) ? browserLang : 'en');
   }
 }
 
 export function translateLoaderFactory(httpClient: HttpClient) {
   return new TranslateHttpLoader(httpClient);
+}
+
+export function translateCacheFactory(
+  translateService: TranslateService,
+  translateCacheSettings: TranslateCacheSettings
+) {
+  return new TranslateCacheService(translateService, translateCacheSettings);
 }
